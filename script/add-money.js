8595b9e802a651de513e8079581c59e41e0686e8
NazmulhaqueD <mdnajmulhaque982@gmail.com>
2025-02-26 12:39:31 +0600

    add money completed

--- script/add-money.js
+++ script/add-money.js
@@ -1,1 +1,22 @@
-alert('hello')
+document.getElementById('add-money')
+    .addEventListener("click", function (event) {
+        event.preventDefault()
+        const amount = parseFloat(document.getElementById('amount').value);
+        const password = parseInt(document.getElementById('password').value)
+        const initialAmount = document.getElementById('initial-amount').innerText
+        const convertInitialAmount = parseFloat(initialAmount);
+        const account = document.getElementById('account-number').value;
+        
+
+        if (password === 1234 && account.length === 11) {
+            const sum = amount + convertInitialAmount;
+            document.getElementById('initial-amount').innerText = sum;
+        }
+        else {
+            alert('Please Enter Your Valid Password')
+        }
+    })
+
+document.getElementById('log-out').addEventListener('click', function () {
+    window.location.href = 'index.html'
+})
